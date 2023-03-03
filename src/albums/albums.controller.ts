88e0660aadd59431e7919dca09df573f42ba0cec
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-03 11:52:20 +0200

    HTTP request methods

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put} from '@nestjs/common';
 
 @Controller('albums')
-export class AlbumsController {}
+export class AlbumsController {
+    @Get()
+        getAlbums(){
+            return [];
+        }
+
+    @Get(':id')
+        getSpecificAlbum(){
+            return {};
+        }
+
+    @Post()
+        addAlbum(){
+            return {};
+        }
+
+    @Put(':id')
+        editAlbum(){
+            return {};
+        }
+
+    @Delete(':id')
+        deleteAlbum(){
+            return {};
+        }
+    
+}
