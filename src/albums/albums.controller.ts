dd1381539f2d7c7310caa60ed5d6b06ffb6e720b
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-03 15:23:36 +0200

    CRUD operations for albums module

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,30 +1,45 @@
-import { Controller, Delete, Get, Post, Put} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query} from '@nestjs/common';
+import { CreateAlbumsDto } from './dto/create-albums.dto';
+import { UpdateAlbumsDto } from './dto/update-albums.dto';
 
 @Controller('albums')
 export class AlbumsController {
     @Get()
-        getAlbums(){
-            return [];
+        getAlbums(@Query('name') name:string){
+            return [{
+                name
+            }];
         }
 
     @Get(':id')
-        getSpecificAlbum(){
-            return {};
+        getSpecificAlbum(@Param('id') id:string){
+            return {
+                id
+            }; 
         }
 
     @Post()
-        addAlbum(){
-            return {};
+        addAlbum(@Body() addAlbumDto: CreateAlbumsDto){
+            return {
+                name: addAlbumDto.name,
+                releaseYear: addAlbumDto.releaseYear
+            };
         }
 
     @Put(':id')
-        editAlbum(){
-            return {};
+        editAlbum(@Param('id') id:string, @Body() updateAlbumsDto: UpdateAlbumsDto){
+            return {
+                id,
+                name: updateAlbumsDto.name,
+                releaseYear: updateAlbumsDto.releaseYear
+            };
         }
 
     @Delete(':id')
-        deleteAlbum(){
-            return {};
+        deleteAlbum(@Param('id') id:string){
+            return {
+                id
+            };
         }
     
 }
