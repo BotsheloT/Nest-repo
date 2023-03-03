63791b15ee80c15c9515f7a3f835d05c0c610224
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-03 13:53:41 +0200

    New dependant modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumsModule } from './albums/albums.module';
 import { PlaylistsModule } from './playlists/playlists.module';
+import { LikedSongsModule } from './liked-songs/liked-songs.module';
 
 @Module({
-  imports: [AlbumsModule, PlaylistsModule],
+  imports: [AlbumsModule, PlaylistsModule, LikedSongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
