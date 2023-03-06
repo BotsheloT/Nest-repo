99272d4d2f5300931bf67d696e8c795adb3b852e
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-06 08:18:23 +0200

    Albums Service Logic

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AlbumsService {}
+export class AlbumsService {
+    private albums =[
+        {id: 0, name: 'Secrets', genre: 'RnB'},
+        {id: 1, name: 'MM..FOOD', genre: 'Hip Hop'},
+        {id: 2, name: 'Umdali', genre: 'Jazz'}
+      ];
+    
+      getAlbums(genre?: 'Hip Hop' | 'Jazz'){
+        if(genre){
+          return this.albums.filter((albums) => albums.genre === genre);
+        }
+        return this.albums;
+      }
+}
