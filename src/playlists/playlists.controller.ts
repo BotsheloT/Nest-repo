c28a8dc3756cf2dc8e811315509bb8c57cf76f7d
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-07 10:37:27 +0200

    Playlist requests

--- src/playlists/playlists.controller.ts
+++ src/playlists/playlists.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, Post, Query, Delete } from '@nestjs/common';
+import { CreatePlaylistDto } from './dto/create-playlists.dto';
 import { PlaylistsService } from './playlists.service';
 
 @Controller('playlists')
 export class PlaylistsController {
   constructor(private readonly playlistsService: PlaylistsService) {}
+
+  @Get()
+    getPlaylists(@Query('theme') theme:string){
+      return [];
+    }
+
+    @Get(':id')
+      getPlaylist(@Param('id') id:number){
+      return {};
+    }
+
+    @Post()
+      addPlaylist(@Body() createPlaylistDto: CreatePlaylistDto){
+        return{
+          name: createPlaylistDto.name,
+          theme: createPlaylistDto.theme
+        }
+    }    
+
+    @Put('id')
+      editPLaylist(@Param() id:number ){
+        return [];
+    }
+
+    @Delete('id')
+      deletePlaylist(@Param() id:number){
+        return{
+          id
+        }
+      }
 }
