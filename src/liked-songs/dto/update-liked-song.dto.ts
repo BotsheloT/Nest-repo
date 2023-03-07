56ef3684156ef485dcbdcd3bd2c0781c965a68e7
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-07 13:42:53 +0200

    Deleted Module

--- src/liked-songs/dto/update-liked-song.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLikedSongDto } from './create-liked-song.dto';
-
-export class UpdateLikedSongDto extends PartialType(CreateLikedSongDto) {}
--- src/liked-songs/liked-songs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LikedSongsService } from './liked-songs.service';
-import { LikedSongsController } from './liked-songs.controller';
-
-@Module({
-  controllers: [LikedSongsController],
-  providers: [LikedSongsService]
-})
-export class LikedSongsModule {}
