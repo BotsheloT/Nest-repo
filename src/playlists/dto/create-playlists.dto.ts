7716c5e29b002c6f64a727cb7f42d64c7aa1155c
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-07 12:21:47 +0200

    Error message change

--- src/playlists/dto/create-playlists.dto.ts
+++ src/playlists/dto/create-playlists.dto.ts
@@ -1,7 +1,7 @@
 import { IsEnum, MinLength } from "class-validator";
 
 export class CreatePlaylistDto {
-    @MinLength(4, {message: 'Plalist name is too short'})
+    @MinLength(4, {message: 'Playlist name is too short'})
     name: string;
     @IsEnum(['Chill', 'Hype'], {message: 'Your playlist should only be Chill or Hype'})
     theme: string;
