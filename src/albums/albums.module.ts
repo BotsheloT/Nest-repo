5754e8e087d2308593846e210c816b0290c76047
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-08 14:29:22 +0200

    Schema intergration

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
+import { album, albumSchema} from './schemas/album.schema';
 
 @Module({
+  imports:[MongooseModule.forFeature([{name: album.name, schema: albumSchema}])],
   controllers: [AlbumsController],
   providers: [AlbumsService]
 })
--- src/playlists/playlists.module.ts
+++ src/playlists/playlists.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlaylistsService } from './playlists.service';
 import { PlaylistsController } from './playlists.controller';
+import {playlist, playlistSchema} from './schemas/playlist.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: playlist.name, schema: playlistSchema}])],
   controllers: [PlaylistsController],
   providers: [PlaylistsService]
 })
