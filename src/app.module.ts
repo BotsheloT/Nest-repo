50e948c22679431dc68440319bd83bcf1de00ec2
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2023-03-09 08:08:19 +0200

    Connection String change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumsModule } from './albums/albums.module';
 import { PlaylistsModule } from './playlists/playlists.module';
 
-@Module({
-  // , MongooseModule.forRoot('mongodb+srv://main:<277Black353>@cluster0.ypaxhzy.mongodb.net/?retryWrites=true&w=majority')
-  imports: [AlbumsModule, PlaylistsModule],
+@Module({  
+  imports: [AlbumsModule, PlaylistsModule, MongooseModule.forRoot('mongodb+srv://main:277Black353@cluster0.ypaxhzy.mongodb.net/test')],
   controllers: [AppController],
   providers: [AppService],
 })
